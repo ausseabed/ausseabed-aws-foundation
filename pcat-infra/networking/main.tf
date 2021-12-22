d0c3fe90183f5af68496cf51d80c3eb9ca49141a
Reuben Turk <reuben.turk@ga.gov.au>
2021-12-22 15:05:07 +1100

    ASP-49 Add SSL certificate for the MH370 API.

--- pcat-infra/networking/main.tf
+++ pcat-infra/networking/main.tf
@@ -45,8 +45,6 @@ data "aws_subnet_ids" "db_tier_subnets" {
   }
 }
 
-#TODO
-
 locals {
   dns_map = map(
     "default", "dev.ausseabed.gov.au.",
@@ -60,9 +58,13 @@ locals {
   env_domain = local.domain_map[var.env]
 }
 
-
 resource "aws_acm_certificate" "cert" {
   domain_name       = "catalogue.${local.dns_zone}"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate" "cert_mh370api" {
+  domain_name       = "mh370-api.${local.dns_zone}"
   validation_method = "DNS"
 }
 
@@ -88,6 +90,14 @@ resource "aws_route53_record" "ga_sb_pc_route53" {
     zone_id                = aws_lb.ga_sb_pc_load_balancer.zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "cert_mh370api_validation" {
+  name    = aws_acm_certificate.cert_mh370api.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert_mh370api.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.zone.id
+  records = [aws_acm_certificate.cert_mh370api.domain_validation_options.0.resource_record_value]
+  ttl     = 60
 }
 
 resource "aws_route53_record" "ga_sb_mh370api_route53" {
@@ -138,10 +148,14 @@ resource "aws_security_group" "ga_sb_env_pc_public_sg" {
   }
 }
 
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "cert_mh370api" {
+  certificate_arn         = aws_acm_certificate.cert_mh370api.arn
+  validation_record_fqdns = [aws_route53_record.cert_mh370api_validation.fqdn]
 }
 
 resource "aws_lb" "ga_sb_pc_load_balancer" {
@@ -165,6 +179,11 @@ resource "aws_lb_listener" "ga_sb_pc_load_balancer_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ga_sb_pc_load_balancer_outside.arn
   }
+}
+
+resource "aws_lb_listener_certificate" "ga_sb_mh370api_load_balancer_certificate" {
+  listener_arn    = aws_lb_listener.ga_sb_pc_load_balancer_listener.arn
+  certificate_arn = aws_acm_certificate.cert_mh370api.arn
 }
 
 resource "aws_lb_listener_rule" "ga_sb_mh370api_load_balancer_listener" {
