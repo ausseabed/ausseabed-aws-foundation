ea17f51b0f436fc1f38de1571969f56df706a23a
Reuben Turk <reuben.turk@ga.gov.au>
2022-01-18 13:35:04 +1100

    ASP-62 Added staging bucket for build process dependencies and VPC peering for oracle.

--- infra/networking/40_vpc_peering.tf
+++ infra/networking/40_vpc_peering.tf
@@ -1,3 +1,4 @@
+# eGIS database peering.
 data "aws_secretsmanager_secret" "egis_vpc_peering" {
   count = var.env == "prod" ? 1 : 0
 
@@ -37,4 +38,46 @@ resource "aws_route" "egis_web_vpc_route" {
   route_table_id            = aws_route_table.ga_sb_vpc_public_route_table.id
   destination_cidr_block    = jsondecode(data.aws_secretsmanager_secret_version.egis_vpc_peering[count.index].secret_string)["cidr_block"]
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering_connection[count.index].id
+}
+
+# Oracle database peering.
+data "aws_secretsmanager_secret" "oracle_vpc_peering" {
+  count = var.env == "prod" ? 0 : 1
+
+  name = "oracle_vpc_peering"
+}
+
+data "aws_secretsmanager_secret_version" "oracle_vpc_peering" {
+  count = var.env == "prod" ? 0 : 1
+
+  secret_id = data.aws_secretsmanager_secret.oracle_vpc_peering[count.index].id
+}
+
+resource "aws_vpc_peering_connection" "oracle_vpc_peering_connection" {
+  count = var.env == "prod" ? 0 : 1
+
+  peer_vpc_id   = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["vpc_id"]
+  vpc_id        = aws_vpc.ga_sb_vpc.id
+  peer_region   = "ap-southeast-2"
+  peer_owner_id = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["account_id"]
+
+  tags = {
+    Name = "oracle-vpc-peer-${var.env}"
+  }
+}
+
+resource "aws_route" "oracle_app_vpc_route" {
+  count = var.env == "prod" ? 0 : 1
+
+  route_table_id            = aws_route_table.ga_sb_vpc_nat_route_table.id
+  destination_cidr_block    = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["cidr_block"]
+  vpc_peering_connection_id = aws_vpc_peering_connection.oracle_vpc_peering_connection[count.index].id
+}
+
+resource "aws_route" "oracle_web_vpc_route" {
+  count = var.env == "prod" ? 0 : 1
+
+  route_table_id            = aws_route_table.ga_sb_vpc_public_route_table.id
+  destination_cidr_block    = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["cidr_block"]
+  vpc_peering_connection_id = aws_vpc_peering_connection.oracle_vpc_peering_connection[count.index].id
 }
--- wh-infra/s3/main.tf
+++ wh-infra/s3/main.tf
@@ -5,3 +5,22 @@ resource "aws_s3_bucket" "mh370_cache" {
 resource "aws_s3_bucket" "mh370_storymap" {
   bucket = "ausseabed-mh370-storymap-${var.env}"
 }
+
+# Bucket for files required to build environments. Build processes can pull from here.
+resource "aws_s3_bucket" "staging" {
+  bucket = "ausseabed-staging-${var.env}"
+
+  cors_rule {
+    allowed_headers = [
+      "*"
+    ]
+    allowed_methods = [
+      "GET"
+    ]
+    allowed_origins = [
+      "*"
+    ]
+    expose_headers  = []
+    max_age_seconds = 3600
+  }
+}
