33ef936411943b3e0c70beafe6dac3179d5e7954
Reuben Turk <reuben.turk@ga.gov.au>
2022-01-18 16:41:38 +1100

    ASP-62 Update Oracle VPC peering to be in prod only.

--- infra/networking/40_vpc_peering.tf
+++ infra/networking/40_vpc_peering.tf
@@ -42,19 +42,19 @@ resource "aws_route" "egis_web_vpc_route" {
 
 # Oracle database peering.
 data "aws_secretsmanager_secret" "oracle_vpc_peering" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "prod" ? 1 : 0
 
   name = "oracle_vpc_peering"
 }
 
 data "aws_secretsmanager_secret_version" "oracle_vpc_peering" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "prod" ? 1 : 0
 
   secret_id = data.aws_secretsmanager_secret.oracle_vpc_peering[count.index].id
 }
 
 resource "aws_vpc_peering_connection" "oracle_vpc_peering_connection" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "prod" ? 1 : 0
 
   peer_vpc_id   = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["vpc_id"]
   vpc_id        = aws_vpc.ga_sb_vpc.id
@@ -67,7 +67,7 @@ resource "aws_vpc_peering_connection" "oracle_vpc_peering_connection" {
 }
 
 resource "aws_route" "oracle_app_vpc_route" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "prod" ? 1 : 0
 
   route_table_id            = aws_route_table.ga_sb_vpc_nat_route_table.id
   destination_cidr_block    = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["cidr_block"]
@@ -75,7 +75,7 @@ resource "aws_route" "oracle_app_vpc_route" {
 }
 
 resource "aws_route" "oracle_web_vpc_route" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "prod" ? 1 : 0
 
   route_table_id            = aws_route_table.ga_sb_vpc_public_route_table.id
   destination_cidr_block    = jsondecode(data.aws_secretsmanager_secret_version.oracle_vpc_peering[count.index].secret_string)["cidr_block"]
